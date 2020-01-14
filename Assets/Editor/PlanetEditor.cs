396ce05c4f4e2bc0a99f5ef28fe64d5fd6723ac0
DanyZy <daniilzgurovec@gmail.com>
2020-01-14 21:54:29 +0600

    implemented settings of planet & editor gui for them

---
+++ Assets/Editor/PlanetEditor.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Planet))]
+public class PlanetEditor : Editor
+{
+    private Planet planet;
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        DrawSettingsEditor(planet.shapeSettings, planet.OnShapeSettingsUpdated);
+        DrawSettingsEditor(planet.colorSettings, planet.OnColorSettingsUpdated);
+    }
+
+    void DrawSettingsEditor(Object settings, System.Action onSettingsUpdated)
+    {
+        using (var check = new EditorGUI.ChangeCheckScope())
+        {
+            Editor editor = CreateEditor(settings);
+            editor.OnInspectorGUI();
+
+            if (check.changed)
+            {
+                onSettingsUpdated();
+            }
+        }
+    }
+
+    private void OnEnable()
+    {
+        planet = (Planet)target;
+    }
+}
--- Assets/Scripts/Planet.cs
+++ Assets/Scripts/Planet.cs
@@ -7,18 +7,24 @@ public class Planet : MonoBehaviour
     [Range(2, 256)]
     public int resolution = 10;
 
+    public ShapeSettings shapeSettings;
+    public ColorSettings colorSettings;
+
+    private ShapeGenerator shapeGenerator;
+
     [SerializeField, HideInInspector]
     private MeshFilter[] meshFilters;
     private TerrainFace[] terrainFaces;
 
     private void OnValidate()
     {
-        Initialize();
-        GenerateMesh();
+        GeneratePlanet();
     }
 
     private void Initialize()
     {
+        shapeGenerator = new ShapeGenerator(shapeSettings);
+
         if (meshFilters == null || meshFilters.Length == 0)
         {
             meshFilters = new MeshFilter[6];
@@ -47,8 +53,29 @@ public class Planet : MonoBehaviour
                 meshFilters[i].sharedMesh = new Mesh();
             }
 
-            terrainFaces[i] = new TerrainFace(meshFilters[i].sharedMesh, resolution, directions[i]);
+            terrainFaces[i] = new TerrainFace(shapeGenerator, meshFilters[i].sharedMesh, resolution, directions[i]);
         }
+    }
+
+    public void GeneratePlanet()
+    {
+        Initialize();
+        GenerateMesh();
+        GenerateColors();
+    }
+
+    private void GenerateColors()
+    {
+        foreach (MeshFilter mf in meshFilters)
+        {
+            mf.GetComponent<MeshRenderer>().sharedMaterial.color = colorSettings.planetColor;
+        }
+    }
+
+    public void OnColorSettingsUpdated()
+    {
+        Initialize();
+        GenerateColors();
     }
 
     private void GenerateMesh()
@@ -57,5 +84,11 @@ public class Planet : MonoBehaviour
         {
             face.ConstructMesh();
         }
+    }
+
+    public void OnShapeSettingsUpdated()
+    {
+        Initialize();
+        GenerateMesh();
     }
 }
